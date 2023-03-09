69c2415d73a5033f293889017ea8de61264b7478
Kitae's Github <aqua0405@gmail.com>
2023-03-10 01:02:17 +0900

    Create CustomCanvas.java

---
+++ src/jgraphx/basic/CustomCanvas.java
@@ -0,0 +1,87 @@
+package jgraphx.basic;
+
+import javax.swing.JFrame;
+
+import com.mxgraph.canvas.mxICanvas;
+import com.mxgraph.canvas.mxImageCanvas;
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.swing.view.mxInteractiveCanvas;
+import com.mxgraph.view.mxCellState;
+import com.mxgraph.view.mxGraph;
+
+public class CustomCanvas extends JFrame
+{
+	public CustomCanvas(String title)
+	{
+		setTitle(title);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400, 320);
+		setLocationRelativeTo(null);
+		
+		// 그래프 컴포넌트 만들기 
+		mxGraphComponent graphComponent = makeGraph();
+		add(graphComponent);
+		
+		setVisible(true);
+	}
+
+	/** 
+	 * 그래프 만들기 
+	 * @return
+	 */
+	private mxGraphComponent makeGraph()
+	{
+		// 그래프 생성
+		mxGraph graph = new mxGraph() {
+			
+			public void drawState(mxICanvas canvas, mxCellState state, boolean drawLabel)
+			{
+				String label = (drawLabel) ? state.getLabel() : "";
+				
+				if (getModel().isVertex(state.getCell())
+						&& canvas instanceof mxImageCanvas
+						&& ((mxImageCanvas) canvas)
+								.getGraphicsCanvas() instanceof SwingCanvas)
+				{
+					((SwingCanvas) ((mxImageCanvas) canvas).getGraphicsCanvas())
+							.drawVertex(state, label);
+				}
+				// Redirection of drawing vertices in SwingCanvas
+				else if (getModel().isVertex(state.getCell())
+						&& canvas instanceof SwingCanvas)
+				{
+					((SwingCanvas) canvas).drawVertex(state, label);
+				}
+				else
+				{
+					super.drawState(canvas, state, drawLabel);
+				}
+			}
+			}
+		};
+		Object parent = graph.getDefaultParent();
+		
+		graph.getModel().beginUpdate();						// 그래프 작성 시작 
+		try {
+			Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80, 30,  "ROUNDED;strokeColor=red;fillColor=green");		// 노드 추가 
+			Object v2 = graph.insertVertex(parent, null, "World", 240, 150, 80, 30, "defaultVertex;fillColor=blue");	// 노드 추가 
+			graph.insertEdge(parent, null, "Edge", v1, v2);								// 간선 추가 
+		} finally {
+			graph.getModel().endUpdate();					// 그래프 작성 종료 
+		}
+		
+		mxGraphComponent graphComponent = new mxGraphComponent(graph);
+		return graphComponent;
+	}
+
+	public class SwingCanvas extends mxInteractiveCanvas
+	{
+		
+	}
+	
+
+	public static void main(String[] args)
+	{
+		CustomCanvas frame = new CustomCanvas("Custom Canvas");		
+	}
+}
