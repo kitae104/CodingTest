268a2219a7d5a3ec656dcb8eddd0d02487ef4219
Kitae's Github <aqua0405@gmail.com>
2023-04-19 02:08:57 +0900

    Create GraphVisual.java

---
+++ src/jgrapht/visualization/GraphVisual.java
@@ -0,0 +1,76 @@
+package jgrapht.visualization;
+
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.nio.Attribute;
+import org.jgrapht.nio.DefaultAttribute;
+import org.jgrapht.nio.dot.DOTExporter;
+
+import java.io.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class GraphVisual {
+
+    public DefaultDirectedGraph<String, DefaultEdge> createGraph() throws IOException {
+
+        DefaultDirectedGraph<String, DefaultEdge> g =
+                new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+
+        String x1 = "x1";
+        String x2 = "x2";
+        String x3 = "x3";
+
+        g.addVertex(x1);
+        g.addVertex(x2);
+        g.addVertex(x3);
+
+        g.addEdge(x1, x2);
+        g.addEdge(x2, x3);
+        g.addEdge(x3, x1);
+
+        return g;
+    }
+
+    private void createGraphViz(DefaultDirectedGraph<String, DefaultEdge> graph) throws IOException {
+        System.out.println("== GraphViz 출력 ==");
+        DOTExporter<String, DefaultEdge> exporter = new DOTExporter<>();
+        exporter.setVertexAttributeProvider((v) ->
+        {
+            Map<String, Attribute> map = new LinkedHashMap<>();
+            map.put("label", DefaultAttribute.createAttribute(v.toString()));
+            return map;
+        });
+
+        // DOT 파일 생성
+        Writer writer = new FileWriter(System.getProperty("user.dir") + "/graphs/graph.dot");
+        exporter.exportGraph(graph, writer);
+
+        // 문자열 형태로 출력
+        Writer w = new StringWriter();
+        exporter.exportGraph(graph, w);
+        System.out.println(w.toString());
+    }
+
+    private void makeGraphImage() throws IOException {
+        // 그림 출력하기
+        String cmd[] = new String[3];
+        cmd[0] = "cmd.exe";
+        cmd[1] = "/C";
+        cmd[2] = "dot -Tpng ./graphs/graph.dot > ./graphs/graph.png";
+
+        Runtime runTime = Runtime.getRuntime();
+        Process process = runTime.exec(cmd);
+    }
+
+    public static void main(String[] args) {
+        GraphVisual gv = new GraphVisual();
+        try {
+            DefaultDirectedGraph<String, DefaultEdge> graph = gv.createGraph();
+            gv.createGraphViz(graph);
+            gv.makeGraphImage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
