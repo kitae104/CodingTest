ef56d3976de46b4ae5426306a69c1ca6dc24863e
Kitae's Github <aqua0405@gmail.com>
2023-04-28 02:44:46 +0900

    Create JavaEditor.java

---
+++ src/app/editor/JavaEditor.java
@@ -0,0 +1,284 @@
+package app.editor;
+
+import org.fife.ui.autocomplete.*;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rtextarea.RTextScrollPane;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.io.*;
+
+public class JavaEditor extends JFrame implements ActionListener {
+
+  private JMenuItem itemExit;
+  private JMenuItem itemInfo;
+  private JMenuItem itemNew;
+  private JMenuItem itemOpen;
+  private JMenuItem itemSave;
+  private RSyntaxTextArea textArea;
+
+  private ImageIcon[] imgs = { new ImageIcon("images/new.png"),
+      new ImageIcon("images/open.png"),
+      new ImageIcon("images/save.png"),
+      new ImageIcon("images/exit.png"),
+      new ImageIcon("images/compile.png"),
+      new ImageIcon("images/run.png")
+  };
+  private JButton btnNew;
+  private JButton btnOpen;
+  private JButton btnSave;
+  private JButton btnExit;
+  private JMenuItem itemFont;
+  private JMenuItem itemCompile;
+  private JMenuItem itemRun;
+  private JButton btnCompile;
+  private JButton btnRun;
+
+  public JavaEditor(String title) {
+    setTitle(title);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setSize(700, 500);
+    setLocationRelativeTo(null);
+    setLayout(new BorderLayout());
+
+    setMenuBar();
+
+    setToolBar();
+
+    setPanCenter();
+
+    setVisible(true);
+  }
+
+  private void setToolBar() {
+    JToolBar toolBar = new JToolBar("내 툴바");
+
+    btnNew = new JButton(imgs[0]);
+    btnNew.addActionListener(this);
+    toolBar.add(btnNew);
+
+    btnOpen = new JButton(imgs[1]);
+    btnOpen.addActionListener(this);
+    toolBar.add(btnOpen);
+
+    btnSave = new JButton(imgs[2]);
+    btnSave.addActionListener(this);
+    toolBar.add(btnSave);
+
+    toolBar.addSeparator();
+
+    btnCompile = new JButton(imgs[4]);
+    btnCompile.addActionListener(this);
+    toolBar.add(btnCompile);
+
+    btnRun = new JButton(imgs[5]);
+    btnRun.addActionListener(this);
+    toolBar.add(btnRun);
+
+    toolBar.addSeparator();
+    btnExit = new JButton(imgs[3]);
+    btnExit.addActionListener(this);
+    btnExit.setToolTipText("프로그램을 종료합니다.");
+    toolBar.add(btnExit);
+
+    add(toolBar, BorderLayout.NORTH);
+  }
+
+  private void setPanCenter() {
+    JPanel panCenter = new JPanel();
+    panCenter.setLayout(new BorderLayout());
+
+    textArea = new RSyntaxTextArea(20, 60);
+    textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+    textArea.setCodeFoldingEnabled(true);
+    RTextScrollPane sp = new RTextScrollPane(textArea);
+
+    CompletionProvider provider = createCompletionProvider();
+    AutoCompletion ac = new AutoCompletion(provider);
+    ac.install(textArea);
+
+    panCenter.add(sp);
+
+    add(panCenter);
+  }
+
+  private CompletionProvider createCompletionProvider() {
+    DefaultCompletionProvider provider = new DefaultCompletionProvider();
+
+    // a straightforward word completion.
+    provider.addCompletion(new BasicCompletion(provider, "abstract"));
+    provider.addCompletion(new BasicCompletion(provider, "assert"));
+    provider.addCompletion(new BasicCompletion(provider, "break"));
+    provider.addCompletion(new BasicCompletion(provider, "case"));
+    provider.addCompletion(new BasicCompletion(provider, "class"));
+    provider.addCompletion(new BasicCompletion(provider, "public"));
+
+    // ... etc ...
+    provider.addCompletion(new BasicCompletion(provider, "transient"));
+    provider.addCompletion(new BasicCompletion(provider, "try"));
+    provider.addCompletion(new BasicCompletion(provider, "void"));
+    provider.addCompletion(new BasicCompletion(provider, "volatile"));
+    provider.addCompletion(new BasicCompletion(provider, "while"));
+    provider.addCompletion(new BasicCompletion(provider, "for"));
+
+    // require the input text to be the same thing as the replacement text.
+    provider.addCompletion(new ShorthandCompletion(provider, "sysout",
+        "System.out.println(", "System.out.println("));
+    provider.addCompletion(new ShorthandCompletion(provider, "syserr",
+        "System.err.println(", "System.err.println("));
+    provider.addCompletion(new ShorthandCompletion(provider, "main",
+        "public static void main(String args[])", "public static void main(String args[])"));
+
+    return provider;
+  }
+
+  private void setMenuBar() {
+    JMenuBar bar = new JMenuBar();
+    JMenu menuFile = new JMenu("파일");
+
+    itemNew = new JMenuItem("새파일");
+    itemNew.addActionListener(this);
+    menuFile.add(itemNew);
+
+    itemOpen = new JMenuItem("열기");
+    itemOpen.addActionListener(this);
+    menuFile.add(itemOpen);
+
+    itemSave = new JMenuItem("저장");
+    itemSave.addActionListener(this);
+    menuFile.add(itemSave);
+
+    menuFile.addSeparator();
+
+    itemExit = new JMenuItem("나가기");
+    itemExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
+    itemExit.addActionListener(this);
+    menuFile.add(itemExit);
+
+    bar.add(menuFile);
+
+    JMenu menuRun = new JMenu("실행");
+    itemCompile = new JMenuItem("컴파일");
+    itemCompile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F5, ActionEvent.CTRL_MASK));
+    itemCompile.addActionListener(this);
+    menuRun.add(itemCompile);
+
+    itemRun = new JMenuItem("실행");
+    itemRun.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, ActionEvent.CTRL_MASK));
+    itemRun.addActionListener(this);
+    menuRun.add(itemRun);
+
+    bar.add(menuRun);
+
+    JMenu menuOption = new JMenu("옵션");
+    itemFont = new JMenuItem("폰트");
+    itemFont.addActionListener(this);
+    menuOption.add(itemFont);
+
+    menuOption.addSeparator();
+
+    JMenuItem itemColor = new JMenuItem("Color");
+    menuOption.add(itemColor);
+
+    bar.add(menuOption);
+
+    JMenu menuInfo = new JMenu("정보");
+    itemInfo = new JMenuItem("프로그램 정보");
+    itemInfo.addActionListener(this);
+    menuInfo.add(itemInfo);
+
+    bar.add(menuInfo);
+
+    setJMenuBar(bar);
+  }
+
+  public static void main(String[] args) {
+    new JavaEditor("Simple Java Editor ver0.1");
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    Object obj = e.getSource();
+    if (obj == itemExit || obj == btnExit) {
+      if ((JOptionPane.showConfirmDialog(
+          this, "정말 나갈까요?",
+          "프로그램 종료", JOptionPane.YES_NO_OPTION,
+          JOptionPane.WARNING_MESSAGE)) == JOptionPane.YES_OPTION) {
+        System.exit(0);
+      }
+    } else if (obj == itemInfo) {
+      JOptionPane.showMessageDialog(
+          this,
+          "간단한 에디터 프로그램",
+          "프로그램 정보",
+          JOptionPane.ERROR_MESSAGE);
+    } else if (obj == itemNew || obj == btnNew) {
+
+    } else if (obj == itemOpen || obj == btnOpen) {
+      fileOpen();
+    } else if (obj == itemSave || obj == btnSave) {
+      fileSave();
+    } else if(obj == itemFont){
+
+    } else if(obj == itemCompile || obj == btnCompile){
+      JOptionPane.showMessageDialog(this, "컴파일");
+    } else if(obj == itemRun || obj == btnRun){
+      JOptionPane.showMessageDialog(this, "실행");
+    }
+  }
+
+  private void fileSave() {
+    JFileChooser fc = new JFileChooser();
+    fc.addChoosableFileFilter(new FileNameExtensionFilter("Java", "java"));
+    fc.addChoosableFileFilter(new FileNameExtensionFilter("TEXT", "txt"));
+    fc.showSaveDialog(this);
+    File out = fc.getSelectedFile();
+
+    BufferedWriter bw = null;
+
+    try {
+      bw = new BufferedWriter(new FileWriter(out));
+      String str = textArea.getText();
+      bw.write(str);
+    } catch (Exception e1) {
+      e1.printStackTrace();
+    } finally {
+      try {
+        bw.close();
+      } catch (IOException e1) {
+        e1.printStackTrace();
+      }
+    }
+  }
+
+  private void fileOpen() {
+    JFileChooser fc = new JFileChooser();
+    fc.addChoosableFileFilter(new FileNameExtensionFilter("Java", "java"));
+    fc.addChoosableFileFilter(new FileNameExtensionFilter("TEXT", "txt"));     
+    fc.showOpenDialog(this);
+
+    File in = fc.getSelectedFile();
+    BufferedReader br = null;
+    String line = null;
+    try {
+      br = new BufferedReader(new FileReader(in));
+      while ((line = br.readLine()) != null) {
+        textArea.append(line + "\n");
+      }
+    } catch (Exception e1) {
+      e1.printStackTrace();
+    } finally {
+      try {
+        br.close();
+      } catch (IOException e1) {
+        e1.printStackTrace();
+      }
+    }
+  }
+
+}
