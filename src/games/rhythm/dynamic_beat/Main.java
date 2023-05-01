891ee5b6bcef4f444ed834be2e3cb1114a208617
Kitae's Github <aqua0405@gmail.com>
2023-05-02 02:18:36 +0900

    Create Main.java

---
+++ src/games/rhythm/dynamic_beat/Main.java
@@ -0,0 +1,10 @@
+package games.rhythm.dynamic_beat;
+
+public class Main {
+
+    public static final int SCREEN_WIDTH = 1280;  // 1280x720 화면 크기 설정
+    public static final int SCREEN_HEIGHT = 720;
+    public static void main(String[] args) {
+        new DynamicBeat();
+    }
+}
