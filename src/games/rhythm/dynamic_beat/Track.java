70468a878bd1cb22790a78aad66f3307dc69df2f
Kitae's Github <aqua0405@gmail.com>
2023-05-08 18:22:42 +0900

    Update Track.java

--- src/games/rhythm/dynamic_beat/Track.java
+++ src/games/rhythm/dynamic_beat/Track.java
@@ -15,15 +15,16 @@ public class Track {
     private String gameImage;   // 해당 곡을 실행했을 때 표지 이미지
     private String startMusic;  // 게임 선택 창에서 해당 곡을 실행했을 때 재생되는 음악
     private String gameMusic;   // 해당 곡을 실행했을 때 재생되는 음악
-
+    private String titleName;   // 곡 제목
     /**
      * Track 클래스의 생성자
      */
-    public Track(String titleImage, String startImage, String gameImage, String startMusic, String gameMusic) {
+    public Track(String titleImage, String startImage, String gameImage, String startMusic, String gameMusic, String titleName) {
         this.titleImage = titleImage;
         this.startImage = startImage;
         this.gameImage = gameImage;
         this.startMusic = startMusic;
         this.gameMusic = gameMusic;
+        this.titleName = titleName;
     }
 }
