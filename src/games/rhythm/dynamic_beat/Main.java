2d3132115ebd21fbe1302ee20469f46b09ca4efa
Kitae's Github <aqua0405@gmail.com>
2023-05-08 23:55:05 +0900

    Update Main.java

--- src/games/rhythm/dynamic_beat/Main.java
+++ src/games/rhythm/dynamic_beat/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static final int SCREEN_WIDTH = 1280;  // 1280x720 화면 크기 설정
     public static final int SCREEN_HEIGHT = 720;
+    public static final int NOTE_SPEED = 7;      // 노트가 떨어지는 속도
+    public static final int SLEEP_TIME = 10;     // 노트가 떨어지는 주기
     public static void main(String[] args) {    	
         new DynamicBeat();
     }
