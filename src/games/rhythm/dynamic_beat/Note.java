3bcdf887cbeb9958baee45f0fc64df5cd5a792c7
Kitae's Github <aqua0405@gmail.com>
2023-05-14 00:31:04 +0900

    Update Note.java

--- src/games/rhythm/dynamic_beat/Note.java
+++ src/games/rhythm/dynamic_beat/Note.java
@@ -6,18 +6,49 @@ import java.awt.*;
 public class Note extends Thread {
 
     private Image noteBasicImage = new ImageIcon("images/new/noteBasic.png").getImage();
-    private int x, y = 580 - (1000 / Main.SLEEP_TIME * Main.NOTE_SPEED);
+    private int x, y = 580 - (1000 / Main.SLEEP_TIME * Main.NOTE_SPEED) * Main.REACH_TIME;
     private String noteType;
+    private boolean proceeded = true;
 
-    public Note(int x, String noteType) {
-        this.x = x;
+    public int getY() {
+        return y;                       // 노트의 y좌표를 반환
+    }
+
+    public String getNoteType() {
+        return noteType;                // 노트 타입을 반환
+    }
+    public boolean isProceeded() {
+        return proceeded;               // 노트가 판정바를 지나갔는지 확인
+    }
+
+    public void close() {
+        proceeded = false;              // 노트가 판정바를 지나갔다면 proceeded를 false로 만듦
+    }
+
+    public Note(String noteType) {
+        if(noteType.equals("S")) {              // 노트 타입이 S면
+            x = 228;
+        } else if(noteType.equals("D")) {       // 노트 타입이 D면
+            x = 332;
+        } else if(noteType.equals("F")) {       // 노트 타입이 F면
+            x = 436;
+        } else if(noteType.equals("Space")) {   // 노트 타입이 Space면
+            x = 540;
+        } else if(noteType.equals("J")) {       // 노트 타입이 J면
+            x = 744;
+        } else if(noteType.equals("K")) {       // 노트 타입이 K면
+            x = 848;
+        } else if(noteType.equals("L")) {       // 노트 타입이 L면
+            x = 952;
+        }
         this.noteType = noteType;
     }
 
     public void screenDraw(Graphics2D g) {
-        if(noteType.equals("short")) {
-            g.drawImage(noteBasicImage, x, y, null);
-        } else if(noteType.equals("long")) {
+        // 노트 타입에 따라 노트 이미지를 그려줌
+        if(!noteType.equals("Space")) {                             // 노트 타입이 Space가 아니면
+            g.drawImage(noteBasicImage, x, y,null);
+        } else {                                                    // 노트 타입이 Space면
             g.drawImage(noteBasicImage, x, y, null);
             g.drawImage(noteBasicImage, x+100, y, null);
         }
@@ -25,6 +56,11 @@ public class Note extends Thread {
 
     public void drop() {
         y += Main.NOTE_SPEED;
+
+        if(y > 620) {                       // 노트가 판정바를 지나가면
+            System.out.println("Miss");
+            close();
+        }
     }
 
     @Override
@@ -32,12 +68,53 @@ public class Note extends Thread {
         try{
             while(true) {
                 drop();
-                Thread.sleep(Main.SLEEP_TIME);
+                if(proceeded) {
+                    Thread.sleep(Main.SLEEP_TIME);
+                } else {
+                    interrupt();        // 해당 스레드를 중지 상태로 만듦
+                    break;
+                }
             }
         }
         catch(Exception e) {
             System.err.println(e.getMessage());
         }
 
+    }
+
+    /**
+     * 판단해주는 메소드
+     */
+    public String judge() {
+        if(y >= 613){                       // 노트가 판정바를 지나가면
+            System.out.println("Late");
+            close();
+            return "Late";
+        } else if(y >= 600){
+            System.out.println("Good");
+            close();
+            return "Good";
+        } else if(y >= 587){
+            System.out.println("Great");
+            close();
+            return "Great";
+        } else if(y >= 573){
+            System.out.println("Perfect");
+            close();
+            return "Perfect";
+        } else if(y >= 565){
+            System.out.println("Great");
+            close();
+            return "Great";
+        } else if(y >= 550){
+            System.out.println("Good");
+            close();
+            return "Good";
+        } else if(y >= 535){
+            System.out.println("Early");
+            close();
+            return "Early";
+        }
+        return "None";      // 판정이 안됐을 경우
     }
 }
